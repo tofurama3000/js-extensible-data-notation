9034b21b3f459d1e100cc134053261557fa082c9
matttolman <tofurama3000@users.noreply.github.com>
2018-12-29 20:47:44 -0700

    Updated tokens

--- src/json_interpreter.ts
+++ src/json_interpreter.ts
@@ -17,7 +17,6 @@ function processToken(token: any): any {
     case 'int':
       return parseInt(data);
     case 'string':
-      return unescapeStr(data);
     case 'char':
       return data;
     case 'keyword':
